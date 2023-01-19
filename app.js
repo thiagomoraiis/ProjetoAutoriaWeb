e798e428c9b08d664588ad4fa3d37f8721aaebca
Thiago Morais Silva <morais.t@academico.ifrn.edu.br>
2023-01-19 17:28:33 -0300

    arrumando o tamanho da navbar ao clicar 2.0

--- app.js
+++ app.js
@@ -9,13 +9,11 @@ window.addEventListener("resize", () => {
 function handleOpenNav() {
     const displayValue = document.getElementById("nav-m").style.display;
     if(displayValue == "block") {
-        document.getElementById("nav-m").style.left = "-1em"
-        document.getElementById("nav-m").style.width = "0"
-        // document.getElementById("nav-m").style.display = "none"
+        document.getElementById("nav-m").style.width = "6em"
+        document.getElementById("nav-m").style.display = "none"
     };
     if(displayValue === "" || displayValue === "none") {
-        document.getElementById("nav-m").style.left = "0"
-        // document.getElementById("nav-m").style.display = "block"
+        document.getElementById("nav-m").style.display = "block"
         document.getElementById("nav-m").style.width = "8em"
     };
 }
